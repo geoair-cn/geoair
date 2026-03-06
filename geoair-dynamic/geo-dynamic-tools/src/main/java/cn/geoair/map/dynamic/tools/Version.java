494cece6b47d6d928ca914f017e4c74fcfc6859b
zhangjun <1159856928@qq.com>
2026-03-06 12:23:43 +0800

    test:遗留项目 springfox参数多一行，springdoc无法展开参数，geoserver集成无法暂时webui Closes feature/23.1.2

--- geoair-dynamic/geo-dynamic-tools/src/main/java/cn/geoair/map/dynamic/tools/Version.java
+++ geoair-dynamic/geo-dynamic-tools/src/main/java/cn/geoair/map/dynamic/tools/Version.java
@@ -7,19 +7,19 @@ import java.util.Set;
 
 public class Version {
 
-    private static final String VERSION = "23.1.2-RC3-SNAPSHOT";
+	private static final String VERSION = "23.1.2-RC3-SNAPSHOT";
 
-    public static String getVersion() {
-        return VERSION;
-    }
+	public static String getVersion() {
+		return VERSION;
+	}
 
-    public static void main(String[] args) {
-        System.out.println("Current version: " + VERSION);
-    }
+	public static void main(String[] args) {
+		System.out.println("Current version: " + VERSION);
+	}
 
-    public static Set<Class<?>> getAllUtils() {
-        return ClassUtil.scanPackage(
-                "cn.geoair.map.dynamic.tools",
-                (clazz) -> !clazz.isInterface() && StrUtil.endWith(clazz.getSimpleName(), "Utils"));
-    }
+	public static Set<Class<?>> getAllUtils() {
+		return ClassUtil.scanPackage("cn.geoair.map.dynamic.tools",
+				(clazz) -> !clazz.isInterface() && StrUtil.endWith(clazz.getSimpleName(), "Utils"));
+	}
+
 }
