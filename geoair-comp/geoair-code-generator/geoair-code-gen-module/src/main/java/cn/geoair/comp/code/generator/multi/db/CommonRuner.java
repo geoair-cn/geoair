046ea45fe6af1d2846b96d3b492ff9fb2c5faf6e
zhangjun <1159856928@qq.com>
2026-03-11 16:48:23 +0800

    代码生成器修改

--- geoair-comp/geoair-code-generator/geoair-code-gen-module/src/main/java/cn/geoair/comp/code/generator/multi/db/CommonRuner.java
+++ geoair-comp/geoair-code-generator/geoair-code-gen-module/src/main/java/cn/geoair/comp/code/generator/multi/db/CommonRuner.java
@@ -10,8 +10,9 @@ import cn.geoair.map.dynamic.adv.spring.AdvExecutorFactory;
 import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.util.StrUtil;
 
-import javax.sql.DataSource;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 /**
  * @author ：张逢吉
@@ -19,82 +20,88 @@ import java.util.List;
  */
 public class CommonRuner {
 
-	DataSource dataSource;
+    DataSource dataSource;
 
-	IAdvExecutor executor;
+    IAdvExecutor executor;
 
-	public CommonRuner(DataSource dataSource) {
-		this.dataSource = dataSource;
-		executor = AdvExecutorFactory.getAdvExecutorByDataSource(dataSource);
-	}
+    public CommonRuner(DataSource dataSource) {
+        this.dataSource = dataSource;
+        executor = AdvExecutorFactory.getAdvExecutorByDataSource(dataSource);
+    }
 
-	public String getTableCommentByTableName(String tableName) {
-		String tableComment;
-		tableComment = executor.dGetTableComment(tableName);
-		return tableComment;
-	}
+    public String getTableCommentByTableName(String tableName) {
+        String tableComment;
+        tableComment = executor.dGetTableComment(tableName);
+        return tableComment;
+    }
 
-	public List<GenTableColumn> getTableColumnsByTableName(String tableName) {
-		// 初始化返回列表
-		List<GenTableColumn> genTableColumnList = ListUtil.list(true);
+    public List<GenTableColumn> getTableColumnsByTableName(String tableName) {
+        // 初始化返回列表
+        List<GenTableColumn> genTableColumnList = ListUtil.list(true);
 
-		// 1. 获取数据库表字段信息
-		DataFieldsApo dataFieldsApo = executor.dGetColumnsByTable(tableName);
-		List<FieldBySchemaApo> dataFieldList = dataFieldsApo.getDataFieldList(true);
+        // 1. 获取数据库表字段信息
+        DataFieldsApo dataFieldsApo = executor.dGetColumnsByTable(tableName);
+        List<FieldBySchemaApo> dataFieldList = dataFieldsApo.getDataFieldList(true);
 
-		// 2. 遍历转换每个字段
-		for (FieldBySchemaApo fieldBySchemaApo : dataFieldList) {
-			GenTableColumn genTableColumn = new GenTableColumn();
-			genTableColumn.setTableName(tableName);
-			// 列名称
-			genTableColumn.setColumnName(fieldBySchemaApo.getColumnName());
-			// 列描述
-			String columnComment = fieldBySchemaApo.getColumnComment();
-			if (StrUtil.isEmpty(columnComment)) {
-				columnComment = "";
-			}
-			else {
-				columnComment = StrUtil.trim(columnComment);
-				columnComment = StrUtil.removeAllLineBreaks(columnComment);
-			}
-			genTableColumn.setColumnComment(columnComment);
-			// 列类型（使用PG的dataType）
-			genTableColumn.setColumnType(fieldBySchemaApo.getDataType());
+        // 2. 遍历转换每个字段
+        for (FieldBySchemaApo fieldBySchemaApo : dataFieldList) {
+            GenTableColumn genTableColumn = new GenTableColumn();
+            genTableColumn.setTableName(tableName);
+            // 列名称
+            genTableColumn.setColumnName(fieldBySchemaApo.getColumnName());
+            // 列描述
+            String columnComment = fieldBySchemaApo.getColumnComment();
+            if (StrUtil.isEmpty(columnComment)) {
+                columnComment = "";
+            } else {
+                columnComment = StrUtil.trim(columnComment);
+                columnComment = StrUtil.removeAllLineBreaks(columnComment);
+            }
+            genTableColumn.setColumnComment(columnComment);
+            // 列类型（使用PG的dataType）
+            genTableColumn.setColumnType(fieldBySchemaApo.getUdtName());
 
-			// Java类型映射
-			genTableColumn.setJavaType(fieldBySchemaApo.getJavaClassName());
-			// 拓展Java类型（暂时和基础类型一致，可根据需要扩展）
-			genTableColumn.setJavaTypeOther(fieldBySchemaApo.getJavaClassName());
+            // Java类型映射
+            genTableColumn.setJavaType(fieldBySchemaApo.getJavaClassName());
+            // 拓展Java类型（暂时和基础类型一致，可根据需要扩展）
+            genTableColumn.setJavaTypeOther(fieldBySchemaApo.getJavaClassName());
 
-			// Java字段名（下划线转驼峰）
-			String columnName = fieldBySchemaApo.getColumnName();
-			String javaField = StrUtil.toCamelCase(columnName);
-			genTableColumn.setJavaField(javaField);
+            // Java字段名（下划线转驼峰）
+            String columnName = fieldBySchemaApo.getColumnName();
+            String javaField = StrUtil.toCamelCase(columnName);
+            genTableColumn.setJavaField(javaField);
 
-			// 是否主键
-			genTableColumn.setIsPk(fieldBySchemaApo.isPrimaryKeyIs() ? "1" : "0");
+            // 是否主键
+            genTableColumn.setIsPk(fieldBySchemaApo.isPrimaryKeyIs() ? "1" : "0");
 
-			genTableColumn.setIsIncrement("0");
-			// 是否必填（isNullable为NO表示必填）
-			genTableColumn.setIsRequired("NO".equals(fieldBySchemaApo.getIsNullable()) ? "1" : "0");
+            genTableColumn.setIsIncrement("0");
+            // 是否必填（isNullable为NO表示必填）
+            genTableColumn.setIsRequired("NO".equals(fieldBySchemaApo.getIsNullable()) ? "1" : "0");
 
-			// 添加到列表
-			genTableColumnList.add(genTableColumn);
-		}
+            // 添加到列表
+            genTableColumnList.add(genTableColumn);
+        }
 
-		return genTableColumnList;
-	}
+        return genTableColumnList;
+    }
 
-	public List<GenTable> selectDbTableListByNames(String[] tableNames) {
-		String sql = "\t\tSELECT\n" + "\t\trelname AS table_name,\n"
-				+ "\t\tobj_description ( C.oid ) AS table_comment\n" + "\t\tFROM\n" + "\t\tpg_class C\n" + "\t\tWHERE\n"
-				+ "\t\tc.relkind IN ('r', 'v')\n"
-				+ "\t\tAND C.relnamespace = ( SELECT oid FROM pg_namespace WHERE nspname = ( SELECT CURRENT_SCHEMA ( ) ) )\n"
-				+ "\t\tAND C.relname NOT LIKE'qrtz_%'\n" + "\t\tAND C.relname NOT LIKE'gen_%'\n"
-				+ "\t\tand C.relname in\n"
-				+ "\t\t<foreach collection=\"array\" item=\"name\" open=\"(\" separator=\",\" close=\")\">\n"
-				+ "\t\t\t#{name}\n" + "\t\t</foreach>";
-		return executor.bSelectObjList(sql, SqlParamMap.of().addOne("array", tableNames), GenTable.class);
-	}
-
+    public List<GenTable> selectDbTableListByNames(String[] tableNames) {
+        String sql =
+                "\t\tSELECT\n"
+                        + "\t\trelname AS table_name,\n"
+                        + "\t\tobj_description ( C.oid ) AS table_comment\n"
+                        + "\t\tFROM\n"
+                        + "\t\tpg_class C\n"
+                        + "\t\tWHERE\n"
+                        + "\t\tc.relkind IN ('r', 'v')\n"
+                        + "\t\tAND C.relnamespace = ( SELECT oid FROM pg_namespace WHERE nspname = ( SELECT CURRENT_SCHEMA ( ) ) )\n"
+                        + "\t\tAND C.relname NOT LIKE'qrtz_%'\n"
+                        + "\t\tAND C.relname NOT LIKE'gen_%'\n"
+                        + "\t\tand C.relname in\n"
+                        + "\t\t<foreach collection=\"array\" item=\"name\" open=\"(\" separator=\",\" close=\")\">\n"
+                        + "\t\t\t#{name}\n"
+                        + "\t\t</foreach>";
+        return executor.bSelectObjList(
+                sql, SqlParamMap.of().addOne("array", tableNames), GenTable.class);
+    }
 }
--- geoair-comp/geoair-code-generator/geoair-code-gen-module/src/main/java/cn/geoair/comp/code/generator/multi/utils/GenConstants.java
+++ geoair-comp/geoair-code-generator/geoair-code-gen-module/src/main/java/cn/geoair/comp/code/generator/multi/utils/GenConstants.java
@@ -1,5 +1,11 @@
 package cn.geoair.comp.code.generator.multi.utils;
 
+import cn.geoair.comp.code.generator.multi.domian.GenTableColumn;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 代码生成通用常量
  *
@@ -54,4 +60,58 @@ public class GenConstants {
 
     /** 时间类型 */
     public static final String TYPE_DATE = "Date";
+
+    private static final Map<String, String> DB_TYPE_TO_JAVA_TYPE = new HashMap<>();
+
+    static {
+        // 高精度小数
+        DB_TYPE_TO_JAVA_TYPE.put("decimal", GenConstants.TYPE_BIGDECIMAL);
+        // 浮点/双精度
+        DB_TYPE_TO_JAVA_TYPE.put("numeric", GenConstants.TYPE_DOUBLE);
+        DB_TYPE_TO_JAVA_TYPE.put("number", GenConstants.TYPE_DOUBLE);
+        DB_TYPE_TO_JAVA_TYPE.put("double", GenConstants.TYPE_DOUBLE);
+        DB_TYPE_TO_JAVA_TYPE.put("float8", GenConstants.TYPE_DOUBLE);
+        DB_TYPE_TO_JAVA_TYPE.put("float4", GenConstants.TYPE_DOUBLE);
+        // 长整型
+        DB_TYPE_TO_JAVA_TYPE.put("int8", GenConstants.TYPE_LONG);
+
+        DB_TYPE_TO_JAVA_TYPE.put("tinyint", GenConstants.TYPE_BYTE);
+        DB_TYPE_TO_JAVA_TYPE.put("smallint", GenConstants.TYPE_INTEGER);
+        DB_TYPE_TO_JAVA_TYPE.put("mediumint", GenConstants.TYPE_INTEGER);
+        DB_TYPE_TO_JAVA_TYPE.put("int", GenConstants.TYPE_INTEGER);
+        DB_TYPE_TO_JAVA_TYPE.put("integer", GenConstants.TYPE_INTEGER);
+        DB_TYPE_TO_JAVA_TYPE.put("int4", GenConstants.TYPE_INTEGER);
+        DB_TYPE_TO_JAVA_TYPE.put("bit", GenConstants.TYPE_BYTE);
+        DB_TYPE_TO_JAVA_TYPE.put("bigint", GenConstants.TYPE_LONG);
+        DB_TYPE_TO_JAVA_TYPE.put("float", GenConstants.TYPE_DOUBLE);
+        DB_TYPE_TO_JAVA_TYPE.put("serial", GenConstants.TYPE_INTEGER);
+        DB_TYPE_TO_JAVA_TYPE.put("bigserial", GenConstants.TYPE_LONG);
+    }
+
+    /**
+     * 转换数据库列类型为 Java 类型
+     *
+     * @param columnType 数据库列类型（如 decimal、int8 等）
+     * @param column 待设置类型的列对象
+     */
+    public static void convertDbTypeToJavaType(String columnType, GenTableColumn column) {
+        // 空值校验（避免空指针）
+        if (columnType == null || column == null) {
+            return;
+        }
+        // 统一转为小写（兼容数据库类型大小写不一致的情况）
+        String lowerColumnType = columnType.trim().toLowerCase();
+
+        // 优先从映射表获取 Java 类型
+        String javaType = DB_TYPE_TO_JAVA_TYPE.get(lowerColumnType);
+        if (javaType != null) {
+            column.setJavaType(javaType);
+            return;
+        }
+
+        // 兜底处理：未匹配到的数字类型默认设为 BigDecimal（防止类型丢失）
+        if (Arrays.asList(COLUMNTYPE_NUMBER).contains(lowerColumnType)) {
+            column.setJavaType(GenConstants.TYPE_BIGDECIMAL);
+        }
+    }
 }
