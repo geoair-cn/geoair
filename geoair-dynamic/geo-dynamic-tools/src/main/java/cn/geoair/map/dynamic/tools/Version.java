4c854bbf027d9ed061cede5bd1f3cbf798466d53
zhangjun <1159856928@qq.com>
2026-03-13 17:17:46 +0800

    feat: 23.1.2-M2版本发布 Closes feature/23.1.2

--- geoair-dynamic/geo-dynamic-tools/src/main/java/cn/geoair/map/dynamic/tools/Version.java
+++ geoair-dynamic/geo-dynamic-tools/src/main/java/cn/geoair/map/dynamic/tools/Version.java
@@ -8,19 +8,19 @@ import java.util.Set;
 
 public class Version {
 
-	private static final String VERSION = "23.1.2-RC3-SNAPSHOT";
+    private static final String VERSION = "23.1.2-M2";
 
-	public static String getVersion() {
-		return VERSION;
-	}
+    public static String getVersion() {
+        return VERSION;
+    }
 
-	public static void main(String[] args) {
-		Gir.log.info("Current version: " + VERSION);
-	}
+    public static void main(String[] args) {
+        Gir.log.info("Current version: " + VERSION);
+    }
 
-	public static Set<Class<?>> getAllUtils() {
-		return ClassUtil.scanPackage("cn.geoair.map.dynamic.tools",
-				(clazz) -> !clazz.isInterface() && StrUtil.endWith(clazz.getSimpleName(), "Utils"));
-	}
+    public static Set<Class<?>> getAllUtils() {
+        return ClassUtil.scanPackage("cn.geoair.map.dynamic.tools" ,
+                (clazz) -> !clazz.isInterface() && StrUtil.endWith(clazz.getSimpleName(), "Utils" ));
+    }
 
 }
