74a17c5e103036c2855af0ec8b8105fa0341e105
zhangjun <1159856928@qq.com>
2026-03-15 12:12:44 +0800

    feat:修复dbserver的api文档的问题 Closes feature/J8.1.0

--- geoair-modules/geoair-apidoc/geoair-knife4j-core/src/main/java/cn/geoair/comp/knife4j/ext/core/config/RunerConfig.java
+++ geoair-modules/geoair-apidoc/geoair-knife4j-core/src/main/java/cn/geoair/comp/knife4j/ext/core/config/RunerConfig.java
@@ -2,7 +2,6 @@ package cn.geoair.comp.knife4j.ext.core.config;
 
 import cn.geoair.base.Gir;
 import cn.geoair.base.util.GutilStr;
-
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.env.Environment;
@@ -17,29 +16,27 @@ import javax.annotation.Resource;
 @Component
 public class RunerConfig implements ApplicationRunner {
 
-	@Resource
-	Environment environment;
+    @Resource
+    Environment environment;
 
-	@Resource
-	GirSwaggerProperties girSwaggerProperties;
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		String path = "localhost:{}";
-		String port = environment.getProperty("server.port");
-		if (port == null) {
-			port = "8080";
-		}
-		String context_path = environment.getProperty("server.servlet.context-path");
-		boolean notBlank = GutilStr.isNotBlank(context_path);
-		if (notBlank) {
-			path = path + context_path;
-		}
-		Gir.log.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-		Gir.log.info("接口文档地址：" + path + "{} 启用状态：{}", port, "/doc.html",
-				// girSwaggerProperties.isEnable());
-				environment.getProperty("geoair.apidoc.enable"));
-		Gir.log.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-	}
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        String path = "localhost:{}";
+        String port = environment.getProperty("server.port");
+        if (port == null) {
+            port = "8080";
+        }
+        String context_path = environment.getProperty("server.servlet.context-path");
+        boolean notBlank = GutilStr.isNotBlank(context_path);
+        if (notBlank) {
+            path = path + context_path;
+        }
+        Gir.log.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        Gir.log.info("接口文档地址：" + path + "{} 启用状态：{}", port, "/doc.html",
+                // girSwaggerProperties.isEnable());
+                environment.getProperty("geoair.apidoc.enable"));
+        Gir.log.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    }
 
 }
