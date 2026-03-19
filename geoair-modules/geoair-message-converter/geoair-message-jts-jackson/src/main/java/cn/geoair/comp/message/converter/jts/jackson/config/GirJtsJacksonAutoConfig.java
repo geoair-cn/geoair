1d398d7820f715fee1042e164665b0f3615daf90
zhangjun <1159856928@qq.com>
2026-03-19 19:44:41 +0800

    fix: 调整依赖 Closes feature/J8.1.0

--- geoair-modules/geoair-message-converter/geoair-message-jts-jackson/src/main/java/cn/geoair/comp/message/converter/jts/jackson/config/GirJtsJacksonAutoConfig.java
+++ geoair-modules/geoair-message-converter/geoair-message-jts-jackson/src/main/java/cn/geoair/comp/message/converter/jts/jackson/config/GirJtsJacksonAutoConfig.java
@@ -1,30 +1,30 @@
 package cn.geoair.comp.message.converter.jts.jackson.config;
 
 import cn.geoair.comp.message.converter.jts.jackson.utils.GirJacksonUtils;
+import cn.hutool.extra.spring.SpringUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+import java.util.Map;
 
-@Configuration
+
 public class GirJtsJacksonAutoConfig implements InitializingBean {
 
     private static final Logger log = LoggerFactory.getLogger(GirJtsJacksonAutoConfig.class);
 
-    @Autowired(required = false)
-    private List<ObjectMapper> objectMappers;
 
     @Override
     public void afterPropertiesSet() {
-        if (objectMappers != null) {
-            for (ObjectMapper objectMapper : objectMappers) {
+        Map<String, ObjectMapper> beansOfType = SpringUtil.getBeansOfType(ObjectMapper.class);
+        if (beansOfType != null) {
+            beansOfType.forEach((name, objectMapper) -> {
                 GirJacksonUtils.registerModule(objectMapper);
-            }
+            });
         }
+
+
     }
 
 
