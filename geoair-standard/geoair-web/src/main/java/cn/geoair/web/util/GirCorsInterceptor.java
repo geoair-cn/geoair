aedfef0435c227cbf0aecb81e31133026b6fd192
zhangjun <1159856928@qq.com>
2026-04-08 14:28:13 +0800

    fix:spark提版本 Closes ${branchName}

--- geoair-standard/geoair-web/src/main/java/cn/geoair/web/util/GirCorsInterceptor.java
+++ geoair-standard/geoair-web/src/main/java/cn/geoair/web/util/GirCorsInterceptor.java
@@ -1,8 +1,8 @@
 package cn.geoair.web.util;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import cn.geoair.base.util.GutilStr;
 import org.springframework.web.servlet.HandlerInterceptor;
