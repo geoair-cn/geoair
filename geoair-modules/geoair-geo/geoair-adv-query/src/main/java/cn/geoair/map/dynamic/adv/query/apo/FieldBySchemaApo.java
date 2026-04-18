a2c92f9c2d723a9d10beef477ed90fb013e69526
zhangjun <1159856928@qq.com>
2026-04-18 11:07:04 +0800

    feat:修复oracle序列化的问题 Closes ${branchName}

--- geoair-modules/geoair-geo/geoair-adv-query/src/main/java/cn/geoair/map/dynamic/adv/query/apo/FieldBySchemaApo.java
+++ geoair-modules/geoair-geo/geoair-adv-query/src/main/java/cn/geoair/map/dynamic/adv/query/apo/FieldBySchemaApo.java
@@ -90,6 +90,7 @@ public class FieldBySchemaApo implements Serializable {
     public boolean isGeometryFieldIs() {
         geometryFieldIs = udtName.equals("geometry")
                 || udtName.equals("geography")
+                || udtName.equals("SDO_GEOMETRY")
                 || udtName.equals("\"public\".\"geometry\"");
         return geometryFieldIs;
     }
