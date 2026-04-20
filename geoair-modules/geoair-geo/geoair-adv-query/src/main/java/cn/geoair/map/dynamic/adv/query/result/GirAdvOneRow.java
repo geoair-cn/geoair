cd17ec428a0d10eef62956eba5ae62e41435a7d6
zhangjun <1159856928@qq.com>
2026-04-20 11:05:06 +0800

    feat:新增驼峰与大小写不敏感的转换 Closes ${branchName}

--- geoair-modules/geoair-geo/geoair-adv-query/src/main/java/cn/geoair/map/dynamic/adv/query/result/GirAdvOneRow.java
+++ geoair-modules/geoair-geo/geoair-adv-query/src/main/java/cn/geoair/map/dynamic/adv/query/result/GirAdvOneRow.java
@@ -3,7 +3,11 @@ package cn.geoair.map.dynamic.adv.query.result;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.collection.ListUtil;
+import cn.hutool.core.map.CamelCaseLinkedMap;
+import cn.hutool.core.map.CamelCaseMap;
+import cn.hutool.core.map.CaseInsensitiveLinkedMap;
 import cn.hutool.db.Entity;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -33,6 +37,36 @@ public class GirAdvOneRow extends LinkedHashMap<String, Object>
         return bean;
     }
 
+    /**
+     * 对key转换成大小写不敏感
+     *
+     * @return
+     */
+    public GirAdvOneRow toCaseInsensitive() {
+        return new GirAdvOneRow(new CaseInsensitiveLinkedMap<String, Object>(this));
+    }
+
+    /**
+     * 对key转换成驼峰的key
+     *
+     * @return
+     */
+    public GirAdvOneRow toCamelCase() {
+        return new GirAdvOneRow(
+                new CamelCaseLinkedMap<String, Object>
+                        (
+                                new CaseInsensitiveLinkedMap<String, Object>
+                                        (this)
+                        )
+        );
+    }
+
+    /**
+     * 转换成简单地map
+     *
+     * @param oneRow
+     * @return
+     */
     public static Map<String, Object> toMap(GirAdvOneRow oneRow) {
         if (oneRow == null) {
             return new LinkedHashMap<>();
@@ -40,6 +74,13 @@ public class GirAdvOneRow extends LinkedHashMap<String, Object>
         return oneRow;
     }
 
+
+    /**
+     * 转换成简单地mapList
+     *
+     * @param rowList
+     * @return
+     */
     public static List<Map<String, Object>> toMapList(List<GirAdvOneRow> rowList) {
         if (rowList == null) {
             return new ArrayList<>();
@@ -50,6 +91,41 @@ public class GirAdvOneRow extends LinkedHashMap<String, Object>
         }
         return list;
     }
+
+    /**
+     * 转换成驼峰的List
+     *
+     * @param rowList
+     * @return
+     */
+    public static List<GirAdvOneRow> toCamelCaseList(List<GirAdvOneRow> rowList) {
+        if (rowList == null) {
+            return new ArrayList<>();
+        }
+        List<GirAdvOneRow> list = new ArrayList<>();
+        for (GirAdvOneRow row : rowList) {
+            list.add(row.toCamelCase());
+        }
+        return list;
+    }
+
+    /**
+     * 转换成大小写不敏感的List
+     *
+     * @param rowList
+     * @return
+     */
+    public static List<GirAdvOneRow> toCaseInsensitiveList(List<GirAdvOneRow> rowList) {
+        if (rowList == null) {
+            return new ArrayList<>();
+        }
+        List<GirAdvOneRow> list = new ArrayList<>();
+        for (GirAdvOneRow row : rowList) {
+            list.add(row.toCaseInsensitive());
+        }
+        return list;
+    }
+
 
     public static List<GirAdvOneRow> ofByEntityList(List<Entity> rows) {
         if (rows == null || rows.isEmpty()) {
@@ -65,6 +141,11 @@ public class GirAdvOneRow extends LinkedHashMap<String, Object>
         return list;
     }
 
+    /**
+     * 通过map的构造函数
+     *
+     * @param map
+     */
     private GirAdvOneRow(Map<String, Object> map) {
         CopyOptions copyOptions = CopyOptions.create();
         copyOptions.setIgnoreNullValue(false);
@@ -72,6 +153,11 @@ public class GirAdvOneRow extends LinkedHashMap<String, Object>
         BeanUtil.copyProperties(map, this, copyOptions);
     }
 
+    /**
+     * 通过hutool的entity创建的
+     *
+     * @param map
+     */
     private GirAdvOneRow(Entity map) {
         CopyOptions copyOptions = CopyOptions.create();
         copyOptions.setIgnoreNullValue(false);
