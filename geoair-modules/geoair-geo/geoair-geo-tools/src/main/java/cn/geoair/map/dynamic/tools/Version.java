e51957c6f0106a2374be3aba0297574bcb6b26e0
zhangjun <1159856928@qq.com>
2026-04-22 10:23:17 +0800

    feat: 修改J17dev的版本为J17-dev-SNAPSHOT Closes ${branchName}

--- geoair-modules/geoair-geo/geoair-geo-tools/src/main/java/cn/geoair/map/dynamic/tools/Version.java
+++ geoair-modules/geoair-geo/geoair-geo-tools/src/main/java/cn/geoair/map/dynamic/tools/Version.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class Version {
 
-    private static final String VERSION = "J17.1.0-SNAPSHOT";
+    private static final String VERSION = "J17-dev-SNAPSHOT";
 
     public static String getVersion() {
         return VERSION;
