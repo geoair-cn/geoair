d4c18735cf46a55ff58d9fa407d3186d51fad74c
zhangjun <1159856928@qq.com>
2026-05-17 19:35:51 +0800

    feat: 新增 AdvWhereLambdaFilter Closes ${branchName}

---
+++ geoair-modules/geoair-geo/geoair-adv-query/src/main/java/cn/geoair/map/dynamic/adv/query/wherequery/AdvWhereLambdaFilter.java
@@ -0,0 +1,9 @@
+package cn.geoair.map.dynamic.adv.query.wherequery;
+
+/**
+ * @author ：张俊
+ * @date ：Created in 2026/5/17 19:27
+ * @description： Lambda 风格的Where 条件构建器
+ */
+public class AdvWhereLambdaFilter {
+}
