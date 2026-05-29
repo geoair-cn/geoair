3311cb0526c5adb5924af9613e889934d277efd9
zhangjun <1159856928@qq.com>
2026-05-29 13:25:00 +0800

    fix: 修复 WITH 语法的读写操作识别 Closes ${branchName}

---
+++ geoair-modules/geoair-dynamic-ds/src/main/java/cn/geoair/comp/dynamic/ds/readwrite/spring/GirDsRdAutoConfiguration.java
@@ -0,0 +1,10 @@
+package cn.geoair.comp.dynamic.ds.readwrite.spring;
+
+/**
+ * @author ：张逢吉
+ * @date ：Created in 2025/10/9 15:28 @description： spring的自动装配
+ */
+public class GirDsRdAutoConfiguration {
+
+
+}
