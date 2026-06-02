06646e937daa3ace5d736a68e353fb7ae4cad9fb
zhangjun <1159856928@qq.com>
2026-06-02 10:38:51 +0800

    fix:新增 读写分离模块单独维护自己的日志 Closes ${branchName}

--- geoair-modules/geoair-dynamic-ds/src/main/java/cn/geoair/comp/dynamic/ds/readwrite/log/RdLog.java
+++ geoair-modules/geoair-dynamic-ds/src/main/java/cn/geoair/comp/dynamic/ds/readwrite/log/RdLog.java
@@ -20,7 +20,7 @@ public class RdLog extends GirLogWrapper {
      * 输出的最小的日志级别 , 这里只是标记输出的最小级别，
      * 这只是第一道拦截器，具体的日志级别还需要看具体的日志实现
      */
-    static GemLogLevel minLogLevel = GemLogLevel.DEBUG;
+    public static GemLogLevel minLogLevel = GemLogLevel.DEBUG;
 
 
     protected void recordLog(GemLogLevel level, String message, LoggerInfo loggerInfo) {
