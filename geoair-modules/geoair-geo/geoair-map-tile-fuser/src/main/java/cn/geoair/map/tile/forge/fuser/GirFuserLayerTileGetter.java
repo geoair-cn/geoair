7341ab2fedaf40000f9e85c063351dca32d04f52
zhangjun <1159856928@qq.com>
2026-06-16 14:41:20 +0800

    feat:  新增瓦片融合实现 Closes ${branchName}

--- geoair-modules/geoair-geo/geoair-map-tile-fuser/src/main/java/cn/geoair/map/tile/forge/fuser/GirFuserLayerTileGetter.java
+++ geoair-modules/geoair-geo/geoair-map-tile-fuser/src/main/java/cn/geoair/map/tile/forge/fuser/GirFuserLayerTileGetter.java
@@ -2,13 +2,16 @@ package cn.geoair.map.tile.forge.fuser;
 
 
 import cn.geoair.base.Gir;
+import cn.geoair.base.util.GutilObject;
+import cn.geoair.map.tile.forge.fuser.cache.TileCacheFactory;
 import cn.geoair.map.tile.forge.fuser.entity.PxyLayerInfo;
 import cn.geoair.map.tile.forge.fuser.provider.LayerTileGetter;
+import cn.geoair.map.tile.forge.fuser.provider.TileGetterFactory;
 
 /**
  * @author ：张俊
  * @date ：Created in 2026/6/15 11:55
- * @description： TODO
+ * @description：  PxyLayerInfo 的获取实现
  */
 public interface GirFuserLayerTileGetter {
 
@@ -16,7 +19,13 @@ public interface GirFuserLayerTileGetter {
         return Gir.beans.getBean(GirFuserLayerTileGetter.class);
     }
 
-    LayerTileGetter getLayerTileGetter(String layerName);
+    default LayerTileGetter getLayerTileGetter(String layerName) {
+        PxyLayerInfo pxyLayerInfo = getPxyLayerInfo(layerName);
+        if (GutilObject.isEmpty(pxyLayerInfo)) {
+            return null;
+        }
+        return TileGetterFactory.create(pxyLayerInfo, TileCacheFactory.getDefaultCache());
+    }
 
     PxyLayerInfo getPxyLayerInfo(String layerName);
 
