c0734e0c518842072039a23af296518a93d58c52
zhangjun <1159856928@qq.com>
2026-06-29 14:37:01 +0800

    修改跨域注解

--- geoair-modules/geoair-geo/geoair-map-tile-forge/src/main/java/cn/geoair/map/tile/forge/core/servlet/HeaderFilter.java
+++ geoair-modules/geoair-geo/geoair-map-tile-forge/src/main/java/cn/geoair/map/tile/forge/core/servlet/HeaderFilter.java
@@ -1,5 +1,6 @@
 package cn.geoair.map.tile.forge.core.servlet;
 
+import cn.geoair.web.util.GirCorsFilter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
@@ -8,40 +9,7 @@ import java.io.IOException;
 
 @Slf4j
 // 不能加Bean注解，否则会自动注册
-public class HeaderFilter implements Filter {
+public class HeaderFilter extends GirCorsFilter {
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
 
-    }
-
-    /**
-     * listScenePageByRoleId
-     * standalone 模式跨域设置
-     *
-     * @param servletRequest
-     * @param servletResponse
-     * @param filterChain
-     * @throws IOException
-     * @throws ServletException
-     */
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-
-        response.setCharacterEncoding("UTF-8");
-        // 跨域设置
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "*");//这里很重要，要不然js header不能跨域携带  Authorization属性
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
-    public void destroy() {
-
-    }
 }
