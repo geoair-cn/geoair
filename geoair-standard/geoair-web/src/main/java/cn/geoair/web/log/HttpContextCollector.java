aa2485ad9fcf9babe2399a0790ab007db1a16cc3
zhangjun <1159856928@qq.com>
2026-07-10 17:43:34 +0800

    合并代码

--- geoair-standard/geoair-web/src/main/java/cn/geoair/web/log/HttpContextCollector.java
+++ geoair-standard/geoair-web/src/main/java/cn/geoair/web/log/HttpContextCollector.java
@@ -1,7 +1,7 @@
 package cn.geoair.web.log;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.function.Consumer;
 
