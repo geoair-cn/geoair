7183f2d0b3d6542e08ce1d7fb74f899f596c8842
zhangjun <1159856928@qq.com>
2026-07-10 15:22:10 +0800

    合并代码

--- geoair-standard/geoair-web/src/main/java/cn/geoair/web/log/HttpContextLoggingFilter.java
+++ geoair-standard/geoair-web/src/main/java/cn/geoair/web/log/HttpContextLoggingFilter.java
@@ -6,9 +6,9 @@ import cn.geoair.web.enums.GirHttpMethod;
 import cn.geoair.web.util.GutilMimeType;
 
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
 
