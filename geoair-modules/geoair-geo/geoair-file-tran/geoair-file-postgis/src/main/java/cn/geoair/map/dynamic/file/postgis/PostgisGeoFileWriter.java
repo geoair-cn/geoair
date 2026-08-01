39e96127833c6e16704f767da9e511e7bd0c25fa
zhangjun <1159856928@qq.com>
2026-08-01 15:38:26 +0800

    javax替换

--- geoair-modules/geoair-geo/geoair-file-tran/geoair-file-postgis/src/main/java/cn/geoair/map/dynamic/file/postgis/PostgisGeoFileWriter.java
+++ geoair-modules/geoair-geo/geoair-file-tran/geoair-file-postgis/src/main/java/cn/geoair/map/dynamic/file/postgis/PostgisGeoFileWriter.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 
 import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.date.StopWatch;
