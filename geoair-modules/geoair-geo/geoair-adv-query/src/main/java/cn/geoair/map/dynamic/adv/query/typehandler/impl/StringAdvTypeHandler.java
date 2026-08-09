85965e72f06da5e7415fb3d67add2dce9f3882ec
zhangjun <1159856928@qq.com>
2026-08-09 20:41:53 +0800

    移除string类型的hander

--- geoair-modules/geoair-geo/geoair-adv-query/src/main/java/cn/geoair/map/dynamic/adv/query/typehandler/impl/StringAdvTypeHandler.java
+++ geoair-modules/geoair-geo/geoair-adv-query/src/main/java/cn/geoair/map/dynamic/adv/query/typehandler/impl/StringAdvTypeHandler.java
@@ -1,29 +1,29 @@
-package cn.geoair.map.dynamic.adv.query.typehandler.impl;
-
-import cn.geoair.map.dynamic.adv.query.typehandler.AdvBaseTypeHandler;
-import cn.geoair.map.dynamic.adv.query.typehandler.AdvTypeHandlerContext;
-
-/**
- * @author ：张逢吉
- * @date ：Created in 2026/7/22
- * @description： 字符串类型处理器
- */
-public class StringAdvTypeHandler extends AdvBaseTypeHandler<String> {
-
-    @Override
-    public boolean supports(Class<?> javaType, Object value) {
-        return javaType != null && CharSequence.class.isAssignableFrom(javaType);
-    }
-
-    @Override
-    protected String convertNonNullForRead(
-            Object value, Class<?> javaType, AdvTypeHandlerContext context) {
-        return String.valueOf(value);
-    }
-
-    @Override
-    protected Object convertNonNullForWrite(
-            String value, Class<?> javaType, AdvTypeHandlerContext context) {
-        return value;
-    }
-}
+//package cn.geoair.map.dynamic.adv.query.typehandler.impl;
+//
+//import cn.geoair.map.dynamic.adv.query.typehandler.AdvBaseTypeHandler;
+//import cn.geoair.map.dynamic.adv.query.typehandler.AdvTypeHandlerContext;
+//
+///**
+// * @author ：张逢吉
+// * @date ：Created in 2026/7/22
+// * @description： 字符串类型处理器
+// */
+//public class StringAdvTypeHandler extends AdvBaseTypeHandler<String> {
+//
+//    @Override
+//    public boolean supports(Class<?> javaType, Object value) {
+//        return javaType != null && CharSequence.class.isAssignableFrom(javaType);
+//    }
+//
+//    @Override
+//    protected String convertNonNullForRead(
+//            Object value, Class<?> javaType, AdvTypeHandlerContext context) {
+//        return String.valueOf(value);
+//    }
+//
+//    @Override
+//    protected Object convertNonNullForWrite(
+//            String value, Class<?> javaType, AdvTypeHandlerContext context) {
+//        return value;
+//    }
+//}
