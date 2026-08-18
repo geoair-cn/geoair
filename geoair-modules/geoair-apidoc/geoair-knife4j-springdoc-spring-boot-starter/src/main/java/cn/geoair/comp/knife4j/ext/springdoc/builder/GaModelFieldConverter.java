17e0239acd0eca74794085837a9880a3bb8993b0
zhangjun <1159856928@qq.com>
2026-08-18 11:20:14 +0800

    新增GirLog4j注解

--- geoair-modules/geoair-apidoc/geoair-knife4j-springdoc-spring-boot-starter/src/main/java/cn/geoair/comp/knife4j/ext/springdoc/builder/GaModelFieldConverter.java
+++ geoair-modules/geoair-apidoc/geoair-knife4j-springdoc-spring-boot-starter/src/main/java/cn/geoair/comp/knife4j/ext/springdoc/builder/GaModelFieldConverter.java
@@ -2,7 +2,7 @@ package cn.geoair.comp.knife4j.ext.springdoc.builder;
 
 import cn.geoair.base.data.model.annotation.GaModel;
 import cn.geoair.base.data.model.annotation.GaModelField;
-import cn.geoair.base.log.GirLog4j;
+
 import cn.hutool.core.util.TypeUtil;
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.core.converter.ModelConverter;
@@ -20,7 +20,7 @@ import org.springframework.core.annotation.AnnotationUtils;
  * @author Administrator
  * @version $Id: $Id
  */
-@GirLog4j
+
 public class GaModelFieldConverter implements ModelConverter {
 
     /** {@inheritDoc} */
