5d8310a93416bd5919727e13f307b708be7e568d
zhangjun <1159856928@qq.com>
2026-08-22 13:43:15 +0800

    提升geotools版本

--- geoair-modules/geoair-geo/geoair-map-tile-forge/src/main/java/cn/geoair/map/tile/forge/core/servlet/MvtTilesServlet.java
+++ geoair-modules/geoair-geo/geoair-map-tile-forge/src/main/java/cn/geoair/map/tile/forge/core/servlet/MvtTilesServlet.java
@@ -11,6 +11,7 @@ import cn.geoair.map.tile.forge.core.TileRequest;
 import cn.geoair.map.tile.forge.core.enums.GirMapTileType;
 import cn.geoair.map.tile.forge.core.model.GirLayerConfigContext;
 import cn.geoair.map.tile.forge.core.service.GirMapTileService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import cn.geoair.web.GirWeb;
 
